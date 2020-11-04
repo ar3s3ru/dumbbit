b675fbd5c57de924a4ef90259f6cecf89b6cfafa
Danilo Cianfrone <danilocianfr@gmail.com>
2020-11-05 00:07:26 +0100

    feat: add hello world http server

--- src/bin/dumbbit.rs
+++ src/bin/dumbbit.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+#[async_std::main]
+async fn main() -> anyhow::Result<()> {
+    let mut app = tide::new();
+
+    app.at("/").get(|_| async move { Ok("Hello, world!\n") });
+
+    Ok(app.listen("0.0.0.0:8080").await?)
 }
